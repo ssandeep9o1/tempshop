3d3191968fe93449280f6b5f43156fc23d6129d3
sathwikreddy122 <venkatasathwik12604@gmail.com>
2026-04-29 12:57:09 +0530

    added item size entity

---
+++ src/main/java/org/example/tempshop/dto/adminDto/AddItemDto.java
@@ -0,0 +1,11 @@
+package org.example.tempshop.dto.adminDto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddItemDto {
+    private String name;
+
+}
--- src/main/java/org/example/tempshop/entity/Items.java
+++ src/main/java/org/example/tempshop/entity/Items.java
@@ -3,18 +3,21 @@ package org.example.tempshop.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.example.tempshop.enums.ItemStatus;
 
 @Getter
 @Setter
 @Entity
 public class Items {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long item_id;
     private String item_name;
     private int stocks;
-    private double price;
-    private boolean availability;
+
+    @Enumerated(EnumType.STRING)
+    private ItemStatus availability;
 
     @ManyToOne
     @JoinColumn(name = "admin_id")
